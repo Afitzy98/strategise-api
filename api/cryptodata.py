1ec617747576bbd910bcb9ccbc0654674d4df320
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2021-01-12 19:12:13 +0000

    fixed bug caused  by key errors

--- api/cryptodata.py
+++ api/cryptodata.py
@@ -16,9 +16,21 @@ def get_ohlcv_data_for_coin(coin_id: str, num_days: int):
   end = get_string_from_timestamp(get_ts_from_time()) # now
   start = get_string_from_timestamp(get_ts_from_time(num_days)) # num days ago
 
-  d = client.candles(coin_id, start=start, end=end)
+  data = client.candles(coin_id, start=start, end=end)
 
-  return [[get_ts_from_string(r["time_open"]), r["open"], r["high"], r["low"], r["close"], r["volume"]] for r in d]
+  out = []
+
+  for row in data:
+    ts = get_ts_from_string(row.get("time_open"))
+    o = row.get("open")
+    h = row.get("high")
+    l = row.get("low")
+    c = row.get("close")
+    v = row.get("volume")
+
+    out.append([ts, o, h, l, c, v])
+
+  return out
 
 
 
